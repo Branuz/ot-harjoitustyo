0062d3d6e8cf47f39fbf3cc716ec6b4680ec614d
Jyri P <jyri.poussu@gmail.com>
2021-11-23 22:00:25 +0200

    Added 2nd test

--- languageApplication/src/test/java/com/poussu/studymate/loginTest.java
+++ languageApplication/src/test/java/com/poussu/studymate/loginTest.java
@@ -1,4 +1,5 @@
 package com.poussu.studymate;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -15,7 +16,7 @@ public class loginTest {
         db = new DataBaseConnection();
         String setup = "CREATE TABLE Users (id INTEGER PRIMARY KEY, email TEXT, name TEXT, password TEXT)";
         db.connect(setup, "CREATE", new String[0]);
-        
+
         String input = "INSERT INTO Users(email, name, password) VALUES (?,?,?)";
         String[] values = {"Admin@gmail.com", "Admin", "1234"};
         db.connect(input, "INSERT", values);
@@ -24,5 +25,9 @@ public class loginTest {
     @Test
     public void userIsFoundInDbWithName(){
         assertTrue(login.checkLoginDetails("Admin", "1234"));
+    }
+    @Test
+    public void notAddedUserIsNotFound(){
+        assertFalse(login.checkLoginDetails("Markka", "23123"));
     }
 }
