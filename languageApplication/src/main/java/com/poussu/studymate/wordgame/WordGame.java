35bb7ddbc637718f26411205e6d3cbae1085af88
Jyri P <jyri.poussu@gmail.com>
2021-12-14 21:07:49 +0200

    More javadoc comments

--- languageApplication/src/main/java/com/poussu/studymate/wordgame/WordGame.java
+++ languageApplication/src/main/java/com/poussu/studymate/wordgame/WordGame.java
@@ -9,6 +9,13 @@ import com.poussu.studymate.dictionary.WordList;
 
 public class WordGame {
     
+    
+    /** 
+     * This method will check if the users translation for the given word is correct or not. It requires the word and the translation.
+     * @param input the word thats translation is to be checked
+     * @param translation the translation for the word 
+     * @return boolean
+     */
     public boolean translationCheck(Word input, String translation) {
         if (input.getTranslation().equals(translation)) {
             return true;
@@ -16,6 +23,12 @@ public class WordGame {
         return false;
     }
 
+    
+    /** 
+     * This method will make a randomised list of words by combining all the words from the lists that are chosen
+     * @param list is the list that contains the names of the lists that will be used for mixed list creation
+     * @return ArrayList<Word>
+     */
     public ArrayList<Word> randomWordList(ArrayList<String> list) {
         EveryList el = new EveryList();
         ArrayList<Word> randomWords = new ArrayList<>();
